adf76d6d8ce06abcfaafea36b0cd7efda8f3fcf7
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-07 21:35:30 -0700

    Update MainActivity.kt

--- app/src/main/java/io/canccode/aca/MainActivity.kt
+++ app/src/main/java/io/canccode/aca/MainActivity.kt
@@ -2,17 +2,27 @@ package io.canccode.aca
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import io.canccode.aca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Load FileBrowserFragment
+        // Load FileBrowserFragment by default
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, FileBrowserFragment())
-                .commit()
+            supportFragmentManager.commit {
+                replace(binding.topContainer.id, FileBrowserFragment())
+            }
+        }
+
+        // LLM Fragment is always mounted
+        supportFragmentManager.commit {
+            replace(binding.llmContainer.id, LLMFragment())
         }
     }
 }
