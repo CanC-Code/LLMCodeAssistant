22308606f322aeffddf0369ca67f3f8dc3712f41
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-07 20:29:02 -0700

    Update MainActivity.kt

--- app/src/main/java/io/canccode/aca/MainActivity.kt
+++ app/src/main/java/io/canccode/aca/MainActivity.kt
@@ -1,69 +1,46 @@
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.drawerlayout.widget.DrawerLayout
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    android:id="@+id/drawer_layout"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent">
+package io.canccode.aca
 
-    <!-- Main content -->
-    <LinearLayout
-        android:orientation="vertical"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent">
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import io.canccode.aca.databinding.ActivityMainBinding
 
-        <!-- Editor container -->
-        <FrameLayout
-            android:id="@+id/editor_container"
-            android:layout_width="match_parent"
-            android:layout_height="0dp"
-            android:layout_weight="3" />
+class MainActivity : AppCompatActivity() {
 
-        <!-- File browser container -->
-        <FrameLayout
-            android:id="@+id/file_browser_container"
-            android:layout_width="match_parent"
-            android:layout_height="0dp"
-            android:layout_weight="2" />
+    private lateinit var binding: ActivityMainBinding
 
-        <!-- LLM output panel -->
-        <FrameLayout
-            android:id="@+id/llm_container"
-            android:layout_width="match_parent"
-            android:layout_height="0dp"
-            android:layout_weight="2" />
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        <!-- LLM input bar -->
-        <LinearLayout
-            android:layout_width="match_parent"
-            android:layout_height="wrap_content"
-            android:orientation="horizontal"
-            android:padding="8dp">
+        // Initialize fragments
+        setupFragments()
+    }
 
-            <EditText
-                android:id="@+id/llm_input_field"
-                android:layout_width="0dp"
-                android:layout_height="wrap_content"
-                android:layout_weight="1"
-                android:hint="Enter prompt"
-                android:imeOptions="actionDone"
-                android:inputType="textMultiLine" />
+    private fun setupFragments() {
+        // Add file browser fragment
+        replaceFragment(R.id.file_browser_container, FileBrowserFragment())
 
-            <ImageButton
-                android:id="@+id/fab_send"
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:src="@android:drawable/ic_menu_send"
-                android:contentDescription="Send" />
-        </LinearLayout>
-    </LinearLayout>
+        // Add code editor fragment
+        replaceFragment(R.id.code_editor_container, CodeEditorFragment())
 
-    <!-- Navigation drawer -->
-    <com.google.android.material.navigation.NavigationView
-        android:id="@+id/nav_view"
-        android:layout_width="wrap_content"
-        android:layout_height="match_parent"
-        android:layout_gravity="start"
-        app:menu="@menu/drawer_menu" />
+        // Add output console fragment
+        replaceFragment(R.id.output_console_container, OutputConsoleFragment())
+    }
 
-</androidx.drawerlayout.widget.DrawerLayout>
+    private fun replaceFragment(containerId: Int, fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(containerId, fragment)
+        }
+    }
+
+    // Example method to update output console
+    fun appendOutput(text: String) {
+        val fragment = supportFragmentManager.findFragmentById(R.id.output_console_container)
+        if (fragment is OutputConsoleFragment) {
+            fragment.appendText(text)
+        }
+    }
+}
