532f1456fe2d8156ce76b15d85e8539a75e52a89
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-08 08:52:12 -0700

    Update EditorFragment.kt

--- app/src/main/java/io/canccode/aca/EditorFragment.kt
+++ app/src/main/java/io/canccode/aca/EditorFragment.kt
@@ -13,32 +13,42 @@ class EditorFragment : Fragment() {
 
     private lateinit var editor: EditText
     private lateinit var saveButton: Button
+    private lateinit var file: File
 
-    private var currentFile: File? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val path = requireArguments().getString(ARG_PATH)!!
+        file = File(path)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_editor, container, false)
+        return inflater.inflate(R.layout.fragment_editor, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         editor = view.findViewById(R.id.editor)
         saveButton = view.findViewById(R.id.save_button)
 
-        saveButton.setOnClickListener {
-            saveFile()
-        }
-
-        return view
-    }
-
-    fun loadFile(file: File) {
-        currentFile = file
         editor.setText(file.readText())
+
+        saveButton.setOnClickListener {
+            file.writeText(editor.text.toString())
+        }
     }
 
-    private fun saveFile() {
-        currentFile?.writeText(editor.text.toString())
+    companion object {
+        private const val ARG_PATH = "path"
+
+        fun newInstance(path: String): EditorFragment {
+            val f = EditorFragment()
+            f.arguments = Bundle().apply {
+                putString(ARG_PATH, path)
+            }
+            return f
+        }
     }
 }
