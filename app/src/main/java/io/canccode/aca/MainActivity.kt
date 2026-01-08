7a8988e900930bc6f6a4662621970fee1c8edf97
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-07 21:21:59 -0700

    Update MainActivity.kt

--- app/src/main/java/io/canccode/aca/MainActivity.kt
+++ app/src/main/java/io/canccode/aca/MainActivity.kt
@@ -2,37 +2,17 @@ package io.canccode.aca
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.canccode.aca.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
-    private val codeEditorFragment = CodeEditorFragment()
-    private val fileBrowserFragment = FileBrowserFragment()
-    private val outputConsoleFragment = OutputConsoleFragment()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // Setup ViewBinding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Load fragments
-        supportFragmentManager.beginTransaction()
-            .replace(binding.codeEditorContainer.id, codeEditorFragment)
-            .replace(binding.fileBrowserContainer.id, fileBrowserFragment)
-            .replace(binding.outputConsoleContainer.id, outputConsoleFragment)
-            .commit()
-    }
-
-    // Convenience methods to interact with OutputConsoleFragment
-    fun appendToConsole(text: String) {
-        outputConsoleFragment.appendText(text)
-    }
-
-    fun clearConsole() {
-        outputConsoleFragment.clear()
+        // Load FileBrowserFragment
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FileBrowserFragment())
+                .commit()
+        }
     }
 }
