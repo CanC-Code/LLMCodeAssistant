9270543b16c8106d77426be11d404d7b90f544d8
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-08 09:25:46 -0700

    Update MainActivity.kt

--- app/src/main/java/io/canccode/aca/MainActivity.kt
+++ app/src/main/java/io/canccode/aca/MainActivity.kt
@@ -2,27 +2,14 @@ package io.canccode.aca
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.top_container, FileBrowserFragment())
-                .commit()
-        }
-    }
-
-    fun openFileInEditor(file: File) {
-        val fragment = EditorFragment.newInstance(file.absolutePath)
-
         supportFragmentManager.beginTransaction()
-            .replace(R.id.top_container, fragment)
-            .addToBackStack(null)
+            .replace(R.id.fragment_container, FileBrowserFragment())
             .commit()
     }
 }
