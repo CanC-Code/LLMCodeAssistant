55a676548b996ee3b5d06ce01351c00cf65d87a0
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-07 21:37:57 -0700

    Create LLMFragment.kt

---
+++ app/src/main/java/io/canccode/aca/LLMFragment.kt
@@ -0,0 +1,44 @@
+package io.canccode.aca
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import io.canccode.aca.databinding.FragmentLlmBinding
+
+class LLMFragment : Fragment() {
+
+    private var _binding: FragmentLlmBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: AppViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLlmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.sendButton.setOnClickListener {
+            val input = binding.inputField.text.toString()
+            viewModel.sendToLLM(input)
+            binding.inputField.text.clear()
+        }
+
+        viewModel.llmInput.observe(viewLifecycleOwner) { input ->
+            // This is where we would send to embedded LLM backend
+            binding.outputField.append("\n> $input")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
