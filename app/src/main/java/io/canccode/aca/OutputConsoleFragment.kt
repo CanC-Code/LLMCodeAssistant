d27a45446f9df0e26c9b09fe5b588a0e7579e21d
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-08 15:13:33 -0700

    Update OutputConsoleFragment.kt

--- app/src/main/java/io/canccode/aca/OutputConsoleFragment.kt
+++ app/src/main/java/io/canccode/aca/OutputConsoleFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ScrollView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import io.canccode.aca.databinding.FragmentOutputConsoleBinding
 
