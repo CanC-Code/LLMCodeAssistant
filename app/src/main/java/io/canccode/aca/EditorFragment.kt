e9b806e72eea1c799a84c05c89a4d5ed0fc30f5f
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-08 22:52:54 -0700

    Update EditorFragment.kt

--- app/src/main/java/io/canccode/aca/EditorFragment.kt
+++ app/src/main/java/io/canccode/aca/EditorFragment.kt
@@ -1,21 +1,22 @@
 package io.canccode.aca
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import io.canccode.aca.databinding.FragmentEditorBinding
 
 class EditorFragment : Fragment() {
 
     private var _binding: FragmentEditorBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: EditorViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentEditorBinding.inflate(inflater, container, false)
@@ -25,13 +26,17 @@ class EditorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.text.observe(viewLifecycleOwner) { text ->
-            binding.editor.setText(text)
-        }
+        binding.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-        binding.editor.addTextChangedListener {
-            viewModel.updateText(it.toString())
-        }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                // handle text changes
+                val text = s?.toString() ?: ""
+                // Example: update a preview or perform an action
+            }
+        })
     }
 
     override fun onDestroyView() {
