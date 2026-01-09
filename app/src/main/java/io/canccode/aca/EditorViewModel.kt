5d88d32446efd591ea16987b05339601aea10bee
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-08 22:40:43 -0700

    Update EditorViewModel.kt

--- app/src/main/java/io/canccode/aca/EditorViewModel.kt
+++ app/src/main/java/io/canccode/aca/EditorViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class EditorViewModel : ViewModel() {
-    private val _text = MutableLiveData("")
+    private val _text = MutableLiveData<String>("")
     val text: LiveData<String> = _text
 
-    fun setText(newText: String) {
+    fun updateText(newText: String) {
         _text.value = newText
     }
 }
