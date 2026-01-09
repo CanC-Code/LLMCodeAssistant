8e0615fd165da35e1ae6d68d0637659f48f26ff3
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-08 22:43:05 -0700

    Update LLMFragment.kt

--- app/src/main/java/io/canccode/aca/LLMFragment.kt
+++ app/src/main/java/io/canccode/aca/LLMFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import io.canccode.aca.databinding.FragmentLlmBinding
@@ -13,12 +12,10 @@ class LLMFragment : Fragment() {
 
     private var _binding: FragmentLlmBinding? = null
     private val binding get() = _binding!!
-
-    private val llmViewModel: LLMViewModel by activityViewModels()
+    private val viewModel: LLMViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLlmBinding.inflate(inflater, container, false)
@@ -28,12 +25,13 @@ class LLMFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Bind TextView from layout
-        val llmOutputText: TextView = binding.llmOutputText
+        viewModel.output.observe(viewLifecycleOwner) { text ->
+            binding.llmOutputText.text = text
+        }
 
-        // Observe LiveData from ViewModel
-        llmViewModel.someLiveData.observe(viewLifecycleOwner) { text ->
-            llmOutputText.text = text
+        binding.llmSendButton.setOnClickListener {
+            val prompt = binding.llmInputText.text.toString()
+            viewModel.setOutput("You typed: $prompt") // temporary echo
         }
     }
 
