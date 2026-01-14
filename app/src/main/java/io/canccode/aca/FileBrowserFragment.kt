cc70cc53271f29d43b2d7843b4f978c5a850d085
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-13 22:54:22 -0700

    Update FileBrowserFragment.kt

--- app/src/main/java/io/canccode/aca/FileBrowserFragment.kt
+++ app/src/main/java/io/canccode/aca/FileBrowserFragment.kt
@@ -1,45 +1,35 @@
-package io.canccode.aca.fragments
+package io.canccode.aca
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import io.canccode.aca.EditorFragment
 import io.canccode.aca.R
-import io.canccode.aca.adapters.FileListAdapter
+import android.widget.FrameLayout
 
 class FileBrowserFragment : Fragment() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: FileListAdapter
+    private lateinit var fragmentContainer: FrameLayout
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_file_browser, container, false)
-        recyclerView = view.findViewById(R.id.recyclerViewFiles)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        adapter = FileListAdapter(getCurrentFileList()) { fileName ->
-            openFile(fileName)
+        fragmentContainer = activity?.findViewById(R.id.fragment_container)!!
+
+        view.findViewById<View>(R.id.someFileItem)?.setOnClickListener {
+            // Example: replace fragment on file click
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, EditorFragment())
+                .addToBackStack(null)
+                .commit()
         }
-        recyclerView.adapter = adapter
 
         return view
-    }
-
-    private fun openFile(fileName: String) {
-        val fragment = EditorFragment.newInstance(fileName)
-        parentFragmentManager.beginTransaction()
-            .replace(R.id.contentContainer, fragment)
-            .addToBackStack(null)
-            .commit()
-    }
-
-    private fun getCurrentFileList(): List<String> {
-        return listOf("Example1.txt", "Example2.txt", "Example3.txt")
     }
 }
