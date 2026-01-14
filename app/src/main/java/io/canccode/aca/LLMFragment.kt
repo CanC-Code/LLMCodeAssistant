2801aa2daec98cb01d8712e7db509e5576ea57d9
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-13 22:54:39 -0700

    Update LLMFragment.kt

--- app/src/main/java/io/canccode/aca/LLMFragment.kt
+++ app/src/main/java/io/canccode/aca/LLMFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import com.llmassistant.llm.LLMHandler
-import io.canccode.aca.EditorFragment
 import io.canccode.aca.R
 
 class LLMFragment : Fragment() {
@@ -16,6 +14,8 @@ class LLMFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Use the correct LLMHandler class
         llmHandler = LLMHandler(requireContext())
     }
 
@@ -25,17 +25,5 @@ class LLMFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_llm, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Example: swap in EditorFragment when LLM is ready
-        val editorFragment = EditorFragment()
-        parentFragmentManager.commit {
-            replace(R.id.fragment_container, editorFragment)
-            setReorderingAllowed(true)
-            addToBackStack(null)
-        }
     }
 }
