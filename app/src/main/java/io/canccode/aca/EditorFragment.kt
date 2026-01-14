f76c19d768a169973ff82cd9cba0495b0b20b6c8
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-13 22:01:17 -0700

    Update EditorFragment.kt

--- app/src/main/java/io/canccode/aca/EditorFragment.kt
+++ app/src/main/java/io/canccode/aca/EditorFragment.kt
@@ -1,35 +1,40 @@
-package io.canccode.aca
+package io.canccode.aca.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import io.canccode.aca.R
 
 class EditorFragment : Fragment() {
 
+    companion object {
+        private const val ARG_FILENAME = "filename"
+
+        fun newInstance(fileName: String) = EditorFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_FILENAME, fileName)
+            }
+        }
+    }
+
+    private lateinit var editorText: EditText
     private var fileName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fileName = arguments?.getString("fileName")
+        fileName = arguments?.getString(ARG_FILENAME)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_editor, container, false)
-    }
-
-    companion object {
-        fun newInstance(fileName: String): EditorFragment {
-            val fragment = EditorFragment()
-            val args = Bundle()
-            args.putString("fileName", fileName)
-            fragment.arguments = args
-            return fragment
-        }
+        val view = inflater.inflate(R.layout.fragment_editor, container, false)
+        editorText = view.findViewById(R.id.editorText)
+        editorText.setText("// Editing: ${fileName ?: "Unknown"}")
+        return view
     }
 }
