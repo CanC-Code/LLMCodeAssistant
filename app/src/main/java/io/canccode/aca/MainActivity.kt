bb34c7cd30cb18786f7de9e4ffde2641f8b591b4
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-13 22:53:58 -0700

    Update MainActivity.kt

--- app/src/main/java/io/canccode/aca/MainActivity.kt
+++ app/src/main/java/io/canccode/aca/MainActivity.kt
@@ -2,19 +2,33 @@ package io.canccode.aca
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.canccode.aca.fragments.FileBrowserFragment
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import android.widget.FrameLayout
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var fragmentContainer: FrameLayout
+    private lateinit var floatingMenuButton: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Load initial fragment
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.contentContainer, FileBrowserFragment())
-                .commit()
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        fragmentContainer = findViewById(R.id.fragment_container)
+        floatingMenuButton = findViewById(R.id.floatingMenuButton)
+
+        floatingMenuButton.setOnClickListener {
+            if (drawerLayout.isDrawerOpen(navView)) {
+                drawerLayout.closeDrawer(navView)
+            } else {
+                drawerLayout.openDrawer(navView)
+            }
         }
     }
 }
