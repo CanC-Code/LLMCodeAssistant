8a824a85676264b6a93432ac02a990af9be8b082
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-01-14 00:17:05 -0700

    Update LLMFragment.kt

--- app/src/main/java/io/canccode/aca/LLMFragment.kt
+++ app/src/main/java/io/canccode/aca/LLMFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import io.canccode.aca.llm.LLMHandler
+import com.llmassistant.llm.LLMHandler
 import io.canccode.aca.R
 
 class LLMFragment : Fragment() {
