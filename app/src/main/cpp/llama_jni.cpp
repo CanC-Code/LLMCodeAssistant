74b28c5939609bde2c543595a95ff8328f9ea76f
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-02-16 22:51:18 -0700

    Update llama_jni.cpp

--- app/src/main/cpp/llama_jni.cpp
+++ app/src/main/cpp/llama_jni.cpp
@@ -91,9 +91,8 @@ Java_io_canccode_aca_LlamaBridge_generateNative(JNIEnv *env, jobject thiz, jstri
 extern "C" JNIEXPORT void JNICALL
 Java_io_canccode_aca_LlamaBridge_clearHistoryNative(JNIEnv *env, jobject thiz) {
     if (ctx) {
-        // Sequence -1 = all sequences
-        // From pos 0 to -1 = all positions
-        llama_kv_cache_seq_rm(ctx, -1, 0, -1);
+        // Updated to the most robust "clear all" function
+        llama_kv_cache_clear(ctx);
     }
 }
 
